0717c5ec02cc057fb9dca857a195e65d6d209588
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-09-06 19:11:30 -1000

    done

--- server.js
+++ server.js
@@ -1,46 +1,29 @@
-const express = require('express');
-const app = express();
-//for io to interact with it .
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const uuid = require('uuid');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
-const cors = require('cors');
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.use(cors())
-
-//setting the template engine
-app.set("view engine","ejs");
-app.use(express.static('public'));
-app.get('/',(req,res)=>{
-
-res.redirect(`/${uuid.v4()}`)
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room',(req,res)=>{
-
-res.render('room',{roomId: req.params.room});
-    
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
-
 
 io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
 
-    socket.on('join-room', (roomId,userId) =>{
-        console.log(`${roomId} ${userId}`);
-        //that socket/user joined the room 
-        socket.join(roomId);
-        //message all existing one when joined user-connected is an event 
-        socket.to(roomId).broadcast.emit('user-connected',userId);
-
-        socket.on('disconnected', () =>{
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
-
-
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
-
+  })
 })
 
-
-server.listen(5003);
+server.listen(5003)
