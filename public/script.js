09a5b10d40da361db0314341104c1044a97820a3
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-09-06 15:12:56 -1000

    issue

--- public/script.js
+++ public/script.js
@@ -1,14 +1,74 @@
 const socket = io('/');
+const vid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined,{
 
     host: '/',
     port: 3001
 
 });
-socket.emit('join-room',RoomId,10);
+const peers = {}
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio: true
+}).then(stream =>{
+
+addVideoStream(myVideo, stream);
+
+myPeer.on('call', call =>{
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        addVideoStream(video,userVideoStream)
+    })
+})
 
 
+
+socket.on('user-connected', userId =>{
+    connectedToNewUser(userId, stream)
+})
+
+})
+/*
+socket.on('user-disconnected', () =>{
+    console.log(userId);
+    if(peers[userId]) peers[userId].close()
+})
+*/
+myPeer.on('open', id =>{
+    socket.emit('join-room',RoomId,id);
+})
+
+
+//on this event of user-connected pass the userId 
 socket.on('user-connected' ,(userId)=>{
     console.log(`user connected ${userId}`);
 })
+
+function connectedToNewUser(userId,stream){
+const call = myPeer.call(userId,stream)
+call.on(stream,userVideoStream =>{
+    const video = document.createElement('video');
+    addVideoStream(video, userVideoStream);
+})
+
+call.on('close', () =>{
+    video.remove();
+})
+    peers[userId] = call ;
+}
+
+
+function addVideoStream(video,stream){
+
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () =>{
+        video.play()
+    })
+    vid.append(video);
+}
 
--- server.js
+++ server.js
@@ -25,9 +25,17 @@ res.render('room',{roomId: req.params.room});
 io.on('connection', socket => {
 
     socket.on('join-room', (roomId,userId) =>{
-        console.log(`${roomId} ${userId}`)
-        socket.join(roomId)
+        console.log(`${roomId} ${userId}`);
+        //that socket/user joined the room 
+        socket.join(roomId);
+        //message all existing one when joined user-connected is an event 
         socket.to(roomId).broadcast.emit('user-connected',userId);
+
+        socket.on('disconnected', () =>{
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+
+
     })
 
 })
